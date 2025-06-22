3f5068364aa64c1d0cdb9e143ee41bf541222a24
manojLovesCoding <mkum9417@gmail.com>
2025-06-22 14:45:51 +0530

    built OTP Model

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -18,6 +18,7 @@ import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { createAccount } from "@/lib/actions/user.actions";
+import OtpModal from "@/components/OTPModal";
 
 type FormType = "sign-in" | "sign-up";
 
@@ -165,6 +166,10 @@ const AuthForm = ({ type }: { type: FormType }) => {
           </div>
         </form>
       </Form>
+
+      {accountId && (
+        <OtpModal email={form.getValues("email")} accountId={accountId} />
+      )}
     </>
   );
 };
