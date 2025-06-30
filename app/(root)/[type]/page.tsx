39c62b4de24453d677b8790e49237b8600d4805f
manojLovesCoding <mkum9417@gmail.com>
2025-06-30 13:40:29 +0530

    built searchComponent

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -1,12 +1,16 @@
 import Card from "@/components/Card";
 import Sort from "@/components/Sort";
 import { getFiles } from "@/lib/actions/file.actions";
+import { getFileTypesParams } from "@/lib/utils";
 import { Models } from "node-appwrite";
 import React from "react";
 
 const Page = async ({ searchParams, params }: SearchParamProps) => {
   const type = ((await params)?.type as string) || "";
-  const files = await getFiles({});
+  const searchText = ((await searchParams)?.query as string) || "";
+  const sort = ((await searchParams)?.sort as string) || "";
+  const types = getFileTypesParams(type) as FileType[];
+  const files = await getFiles({ types, searchText, sort });
   return (
     <div className="mx-auto flex w-full max-w-7xl flex-col items-center gap-8 !important">
       <section className="w-full">
